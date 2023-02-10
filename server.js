1e1123ba5fbe75d5a80b8b8473f2bd4ff26f0e69
Empire <empire@Edis-MBP.lan>
2023-02-10 17:25:33 +0100

    Afresh

--- server.js
+++ server.js
@@ -17,21 +17,15 @@ connectDB()
 
 app.use(logger)
 
-// app.use((req,res,next)=>{
-//     res.header('Access-Control-Allow-Credentials', true)
-//     res.header('Access-Control-Allow-Origin', 'https://dbcheck.onrender.com');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE', 'HEAD');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization', 'X-Requested-With', 'Accept');
-//     next();
-// })
+app.use(cors())
 
-app.use(cors({
-    allowedHeaders: ['Authorization', 'X-Requested-With', 'Content-Type', 'Accept'],
-    allowMethods:['GET','POST','PUT','PATCH','DELETE','HEAD'],
-    origin:'https://dbcheck.onrender.com',
-    credentials: true,
-    optionsSuccessStatus: 200
-}))
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header(`'Access-Control-Allow-Origin', 'https://dbcheck-${api}.onrender.com'`);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE', 'HEAD');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization', 'X-Requested-With', 'Accept');
+    next();
+})
 
 app.use(express.json())
 app.use(cookieParser())
